3865776e8e302b97aadbdd22c8649598042bfc7e
DGY-F <alfathirapridiafaris@gmail.com>
2024-10-02 10:17:24 +0700

    Add files via upload

---
+++ rabu sekarang.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int i,n,jumlah;
+i = 1;
+
+cout << "masukan nilai :" ;
+cin>> n ;
+
+while (i < n){
+	
+	cout<< i*7 << " + " ;
+	jumlah = jumlah + (i*7);
+	i++;
+}
+jumlah - jumlah+(i*7);
+cout << i*7 << " " ;
+cout << " = " << jumlah;
+}
+
+
